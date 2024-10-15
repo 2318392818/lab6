d101ffbe2e19a822d0209ec49539fac59a589d70
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-15 15:35:12 +0800

    引入新的中间件和路由处理

--- server2.js
+++ server2.js
@@ -11,6 +11,10 @@ const logger = (req, res, next) => {
         console.log(`${req.method} ${req.url}`);
         next(); // 调⽤下⼀个中间件
         };
+        const jsonMiddleware = (req, res, next) => {
+                res.setHeader('Content-Type', 'application/json;charset=utf-8');
+                next();
+                };
 // 创建HTTP服务器实例
 const server = createServer((req, res) => {
         logger(req,res,()=>{
@@ -33,6 +37,29 @@ const server = createServer((req, res) => {
             }
     res.end();
     }
+    const getUsersHandler = (req, res) => {
+        res.write(JSON.stringify(users));
+        res.end();
+        };
+        // 路由处理 GET /api/users/:id
+        const getUserHandler = (req, res) => {
+        const id = req.url.split('/')[3];
+        const user = users.find(user => user.id == id);
+        if(user) {
+        res.write(JSON.stringify(user));
+        } else {
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: '⽤户不存在' }));
+        }
+        res.end();
+        };
+        // 路由处理 404
+        const notFoundHandler = (req, res) => {
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: 'API不存在'
+        }));
+        res.end();
+        };
    });
 });
 // 服务器监听端⼝号，且启动成功后输出提⽰信息
